a38ce454100aca3434ed6d12bdb954db349c9871
SSolovev <solovyev87@gmail.com>
2014-11-22 13:45:11 +0300

    check inheritance and polymorph

--- javaPractice/javaPractice/src/main/java/practice/app/App.java
+++ javaPractice/javaPractice/src/main/java/practice/app/App.java
@@ -1,5 +1,8 @@
 package practice.app;
 
+import practice.app.model.Apple;
+import practice.app.model.food.Fruit;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!" );
+        Apple a= new Apple();
+        a.printFruit();
+        Fruit b= new Apple();
+        b.printFruit();
     }
 }
---
+++ javaPractice/javaPractice/src/main/java/practice/app/model/food/Fruit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice.app.model.food;
+
+/**
+ *
+ * @author sergey
+ */
+public abstract class Fruit {
+
+    public Fruit() {
+        System.out.println("Fruit!");
+    }
+
+    public void printFruit() {
+        System.out.println("THIS IS ABSTRACT FRUIT");
+    }
+
+}
