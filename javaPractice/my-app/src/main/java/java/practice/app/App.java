61524fd230640241d13747dd8a64c316a3d5cc4d
SSolovev <solovyev87@gmail.com>
2014-11-22 12:36:15 +0300

    initional

---
+++ javaPractice/my-app/src/main/java/java/practice/app/App.java
@@ -0,0 +1,13 @@
+package java.practice.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
