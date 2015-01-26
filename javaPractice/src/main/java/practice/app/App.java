d4dc999ff173b528b0e7457f6566c364b250db2b
ssolovyev <ssolovyev@at-consulting.ru>
2015-01-26 20:19:23 +0300

    Added arithmetic check

--- javaPractice/src/main/java/practice/app/App.java
+++ javaPractice/src/main/java/practice/app/App.java
@@ -11,46 +11,58 @@ import java.util.regex.Pattern;
 
 /**
  * Hello world!
- *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        int i = 1 >> 2 + 11 - 2 - 11 << 1;
-        System.out.println("i: "+i);
+public class App {
+    public static void main(String[] args) {
+        arithmeticCheck();
         regexpCheck();
     }
 
-    private static void localeCheck(){
+    private static void localeCheck() {
         Locale.setDefault(new Locale("en_US"));
-        Locale locale = new Locale("ru","RU");
-        Locale locale2 = new Locale("tu","RU");
+        Locale locale = new Locale("ru", "RU");
+        Locale locale2 = new Locale("tu", "RU");
 
         ResourceBundle rb = ResourceBundle.getBundle("Bundle", locale2);
 
         System.out.println(rb.getString("testKey"));
     }
 
-    private static void inheritanceCheck(){
-        System.out.println( "Hello World!!" );
-        Apple a= new Apple();
+    private static void inheritanceCheck() {
+        System.out.println("Hello World!!");
+        Apple a = new Apple();
         a.printFruit();
-        Fruit b= new Apple();
+        Fruit b = new Apple();
         b.printFruit();
     }
 
-    private static void regexpCheck(){
-        String patternString="\\d*";
-        String checkString="dw123 678sd 8 9432";
+    private static void regexpCheck() {
+        String patternString = "\\d*";
+        String checkString = "dw123 678sd 8 9432";
         Pattern p = Pattern.compile(patternString);
         Matcher m = p.matcher(checkString);
 //        m.find();
 //        m.matches();
-        while(m.find()){
-            System.out.println(m.start()+" "+m.group());
+        while (m.find()) {
+            System.out.println(m.start() + " " + m.group());
         }
 
+    }
+
+    private static void arithmeticCheck() {
+        int i = 1 >> 2 + 11 - 2 - 11 << 1;
+        System.out.println("i: " + i);
+
+        int a = 6;
+        a += ++a + a++;
+        System.out.println("a: " + a);
+
+        int b = 6;
+        b += b++;
+        System.out.println("b: " + b);
+        b = 6;
+        b = b + b++;
+        System.out.println("b: " + b);
     }
 
 }
