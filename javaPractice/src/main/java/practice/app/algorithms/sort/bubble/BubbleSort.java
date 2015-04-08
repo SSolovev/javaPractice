c25ea4157e5d6674e48d345fdd4817b1f9cc970f
Соловьев Сергей Александрович <ssolovyev@at-consulting.ru>
2015-04-08 16:53:26 +0300

    bubble sort

--- javaPractice/src/main/java/practice/app/algorithms/sort/bubble/BubbleSort.java
+++ javaPractice/src/main/java/practice/app/algorithms/sort/bubble/BubbleSort.java
@@ -4,19 +4,24 @@ package practice.app.algorithms.sort.bubble;
  * Created by SeASolovev on 07.04.2015.
  */
 public class BubbleSort {
-    public static void bubleSort(int[] array){
 
+    public static void bubleSort(int[] array) {
 
-            for (int j = array.length; j > 1; j--) {
-                for (int i = 0; i < array.length-1; i++) {
-                if(array[i]>array[i+1]){
-                    int temp = array[i];
-                    array[i]=array[i+1];
-                    array[i+1]=temp;
+        for (int j = array.length - 1; j > 1; j--) {
+            for (int i = 0; i < j; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i+1);
                 }
             }
 
         }
 
     }
+
+    public static void swap(int[] a, int i1, int i2) {
+        int temp = a[i1];
+        a[i1] = a[i2];
+        a[i2] = temp;
+    }
+
 }
